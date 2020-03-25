d32d54f8ea7d468f597ea7a916a0c8aec6f4357c
Randika97 <randikasuridu@gmail.com>
2020-03-25 15:54:40 +0530

    Ui Complete without Favourit

--- app/src/main/java/com/example/tourist/Login.java
+++ app/src/main/java/com/example/tourist/Login.java
@@ -2,7 +2,9 @@ package com.example.tourist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void Movesignup(View view)
+    {
+        Intent intent = new Intent(this, signup.class);
+        startActivity(intent);
+    }
+    public void Movedashboard(View view)
+    {
+        Intent intent = new Intent(this, dashboard.class);
+        startActivity(intent);
     }
 }
